2965a6f260717e16ecc7302f50a8bf05ef70b414
EnricDul <enric.dulieu97@gmail.com>
2020-02-27 09:53:28 +0100

    project finished

--- script/script.js
+++ script/script.js
@@ -8,4 +8,16 @@ $('.owl-carousel').owlCarousel({
   loop: true,
   margin: 10,
   nav: true,
+  // autoplay: true,
+  autoplayTimeout: 3500,
+  autoplayHoverPause: true,
+  navText: ["<img src='myprevimage.png'>", "<img src='mynextimage.png'>"]
+
+})
+
+$('.play').on('click', function () {
+  owl.trigger('play.owl.autoplay', [2000])
+})
+$('.stop').on('click', function () {
+  owl.trigger('stop.owl.autoplay')
 })
